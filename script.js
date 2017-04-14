60b8fed4dfa78c335c380406ed175db0ce297713
Isabel Dec <idec@id.iit.edu>
2017-04-14 11:07:01 -0500

    Examples of do-while and while.

---
+++ script.js
@@ -0,0 +1,26 @@
+// while loop
+
+document.write("Hello");
+
+for (var dog = 0; dog < 10; dog++) {
+    document.write("<br>");
+    document.write(dog);
+}
+
+
+var cat = 0; 
+do {
+    document.write("<br>");
+    document.write(cat);
+    cat++;
+} while (cat < 10);  
+
+
+
+var monkey = 0;
+while (monkey < 10) {
+    document.write("<br>");
+    document.write(monkey);
+    monkey++;
+}
+
